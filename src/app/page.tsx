d8740413a0d1b491b6d43cad337ed4d47ff393bf
Ketan Solanki <ketangsolanki129@gmail.com>
2023-12-24 11:49:57 +0530

    Page designed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import View from "@/components/view"
+
+
+import { Poppins } from "next/font/google";
+
+
+const poppins = Poppins({
+	subsets: ['latin'],
+	variable: '--font-inter',
+	weight: '600'
+})
+
+
 export default function Home() {
 	return (
-		<main className="">
-			Hello World
+		<main className = "flex flex-col">
+			<div className = "mt-32 text-5xl self-center">
+				<span className = { poppins.className }>Watcher</span>
+			</div>
+
+			<div className = "w-3/4 my-14 py-10 self-center flex justify-evenly flex-wrap">
+				<div className = "w-1/3 my-10 mx-10">
+					<View />
+				</div>
+				<div className = "w-1/3 my-10 mx-10">
+					<View />
+				</div>
+				<div className = "w-1/3 my-10 mx-10">
+					<View />
+				</div>
+				<div className = "w-1/3 my-10 mx-10">
+					<View />
+				</div>
+			</div>
 		</main>
 	)
 }
---
+++ src/components/view.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+
+export default function View(props) {
+    return (
+        <div className = 'h-44 border-4 rounded-xl border-stone-500'>
+            <div>
+                <img
+                    
+                />
+            </div>
+            <div></div>
+        </div>
+    )
+}
